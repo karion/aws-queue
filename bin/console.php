8057d62dd7117553fab382fcb06e5613f45c53ac
Tomasz Szymczyk <karion.ubuntu@gmail.com>
2018-04-01 23:21:57 +0200

    add new commands to console

--- bin/console.php
+++ bin/console.php
@@ -4,14 +4,30 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+use karion\AwsQueue\CliCommand\AwsSqsCreateQueueCommand;
+use karion\AwsQueue\CliCommand\AwsSqsQueueListCommand;
 use Symfony\Component\Console\Application;
 
 $container =  new \League\Container\Container();
 
+$container->delegate(new \League\Container\ReflectionContainer());
+
 $container->add('config', require __DIR__.'/../config/config.php', true);
+
+$dependenciesDir = new DirectoryIterator(__DIR__ . '/../config/container');
+
+foreach (new IteratorIterator($dependenciesDir) as $file) {
+    $filename = $file->getFilename();
+
+    if (substr($filename, -4)=== ".php") {
+        require($file->getPathname());
+    }
+}
 
 $application = new Application();
 
 // ... register commands
+$application->add($container->get(AwsSqsQueueListCommand::class));
+$application->add($container->get(AwsSqsCreateQueueCommand::class));
 
 $application->run();
